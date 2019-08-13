5c4908b99b9da6ae8f7510008cc21a465f509a00
SincereXIA <zhangjunhua@stu.xidian.edu.cn>
2019-08-13 22:23:20 +0800

    change to software gpio for all

--- examples/image.py
+++ examples/image.py
@@ -26,19 +26,20 @@ import Adafruit_GPIO.SPI as SPI
 
 from PIL import Image
 
-
 # Raspberry Pi hardware SPI config:
-DC = 23
-RST = 24
-SPI_PORT = 0
-SPI_DEVICE = 0
+
+#DC = 23
+#RST = 24
+#SPI_PORT = 0
+#SPI_DEVICE = 0
 
 # Raspberry Pi software SPI config:
-# SCLK = 4
-# DIN = 17
-# DC = 23
-# RST = 24
-# CS = 8
+SCLK = 17
+DIN = 18
+DC = 27
+RST = 23
+CS = 22
+
 
 # Beaglebone Black hardware SPI config:
 # DC = 'P9_15'
@@ -46,19 +47,11 @@ SPI_DEVICE = 0
 # SPI_PORT = 1
 # SPI_DEVICE = 0
 
-# Beaglebone Black software SPI config:
-# DC = 'P9_15'
-# RST = 'P9_12'
-# SCLK = 'P8_7'
-# DIN = 'P8_9'
-# CS = 'P8_11'
-
-
 # Hardware SPI usage:
-disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
+# disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
 
 # Software SPI usage (defaults to bit-bang SPI interface):
-#disp = LCD.PCD8544(DC, RST, SCLK, DIN, CS)
+disp = LCD.PCD8544(DC, RST, SCLK, DIN, CS)
 
 # Initialize library.
 disp.begin(contrast=60)
